875ca05c766c5b54c983f9c27da241c2ff745713
Andrew <overfitted@gmail.com>
2024-06-28 22:01:13 +0800

    adds clerk auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,35 @@
+import { SignedOut, SignedIn } from "@clerk/nextjs";
 import { db } from "../server/db";
 
 // prevents caching from showing stale data
 export const dynamic = "force-dynamic"
 
-export default async function HomePage() {
+async function Images() {
   const images = await db.query.images.findMany({
     orderBy:(model, { desc }) => desc(model.id),
   });
-  console.log(images);
 
   return (
+    <div className="flex flex-wrap gap-4">
+      {images.map((image, index) => (
+        <div key={image.id + "-" + index} className="w-48">
+          <img src={image.url} />
+          <div>{image.name}</div>
+        </div>
+      ))}
+    </div>
+  );
+}
+
+export default async function HomePage() {
+  return (
     <main className="">
-      <div className="flex flex-wrap gap-4">
-        {images.map((image, index) => (
-          <div key={image.id + "-" + index} className="w-48">
-            <img src={image.url} />
-            <div>{image.name}</div>
-          </div>
-        ))}
-      </div>
+      <SignedOut>
+        <div className="h-full w-full text-2xl p-4 text-center">Please Sign In.</div>
+      </SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
